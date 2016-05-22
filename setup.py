5f38bbb05cc3bb744fb17f338df6fb0b95c0c394
Benjamin <guangtun.ben.zhu@gmail.com>
2016-05-22 18:32:24 -0400

    setup.py version update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'SetCoverPy',
   packages = ['SetCoverPy'], # this must be the same as the name above
-  version = '0.9.0',
+  version = '0.9.1',
   description = 'A heuristic solver for the set cover problem',
   author = 'Guangtun Ben Zhu',
   author_email = 'guangtun.ben.zhu@gmail.com',
   url = 'https://github.com/guangtunbenzhu/SetCoverPy', # use the URL to the github repo
-  download_url = 'https://github.com/guangtunbenzhu/SetCoverPy/tarball/0.9.0', # I'll explain this in a second
+  download_url = 'https://github.com/guangtunbenzhu/SetCoverPy/tarball/0.9.1', # I'll explain this in a second
   keywords = ['set cover', 'archetype', 'heuristic', 'greedy', 'Lagrangian Relaxation'], # arbitrary keywords
   classifiers = [],
 )
